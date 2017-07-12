ce333f58fbfbcca246144d9bb5bbbba457cb5725
BadSchool <jhunh@naver.com>
2017-07-12 23:08:01 +0900

    텐서플로우 예제(미적분) 입니다.

--- tf_2.py
+++ tf_2.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
+tf.set_random_seed(777)
+
 X = [1,2,3]
 Y = [1,2,3]
 
@@ -11,15 +13,14 @@ cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
 sess = tf.Session()
 
-sess.run(tf.global_variable_initializer())
-
-W_val = []
-cost_val = []
+W_history = []
+cost_history= []
 
 for i in range(-30, 50):
-	feed_W = i * 0.1
-	curr_cost, curr_W = sess.run([cost, W], feed_dict={W: feed_W})
-	W_val.append(curr_cost)
+	curr_W = i * 0.1
+	curr_cost = sess.run(cost, feed_dict={W: curr_W})
+	W_history.append(curr_W)
+	cost_history.append(curr_cost)
 
-plt.plot(W_val, cost_val)
+plt.plot(W_history, cost_history)
 plt.show()
